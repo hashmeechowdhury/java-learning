dc7df8b2ac7d3e864b79b9f79ca59778f362607a
pritomnyc <mahim.pritom@gmail.com>
2021-08-09 17:07:26 -0400

    hello java

---
+++ NumberProblem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NumberProblem {
+
+    public static void main(String[] args) {
+        Scanner go = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        System.out.print("Enter the first decimal number: ");
+        num1 = go.nextDouble();
+        System.out.print("Enter the second decimal number: ");
+        num2 = go.nextDouble();
+
+        System.out.println("You inserted the first decimal number " + num1);
+        System.out.println("You inserted the second decimal number " + num2);
+
+        System.out.println("Sum of your two decimal numbers " + (num1 + num2));
+        System.out.println("Sum of your two decimal numbers " + (num1 - num2));
+        System.out.println("Sum of your two decimal numbers " + (num1 * num2));
+        System.out.println("Sum of your two decimal numbers " + (num1 / num2));
+    }
+
+}
