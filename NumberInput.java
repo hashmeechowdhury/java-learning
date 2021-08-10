50cb99cd0c2a9afa33d870f252172baf23813428
pritomnyc <mahim.pritom@gmail.com>
2021-08-09 23:56:22 -0400

    hello java day1

---
+++ NumberInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumberInput {
+    public static void main(String[] args) {
+        int num1;
+        double num2;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter an integer number: ");
+        num1 = scan.nextInt();
+        System.out.print("Enter a decimal number: ");
+        num2 = scan.nextDouble();
+
+        System.out.println("The integer number is " + num1 + "\n" + "The decimal number is " + num2);
+
+    }
+}
