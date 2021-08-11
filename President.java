be81f6c479f4bc194804f6e191299eca5d7ac3d5
pritomnyc <mahim.pritom@gmail.com>
2021-08-11 13:55:30 -0400

    president

---
+++ President.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class President {
+
+    public static void main(String[] args) {
+
+        int age;
+        String citizen;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Are you a natural born US citizen? (Yes/No) ");
+        citizen = scan.nextLine();
+        System.out.print("Enter your age: ");
+        age = scan.nextInt();
+
+        if (age >= 35 && citizen.equals("Yes")) {
+            System.out.println("You can be President");
+        } else {
+            System.out.println("You cannot be President");
+        }
+        scan.close();
+    }
+
+}
+
+// Cannot compare strings with relational operators
